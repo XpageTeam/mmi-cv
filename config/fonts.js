191b021967ed3f90cab487dca6bea0b5aa6947e8
danil.khandys <danil.khandys@xpage.ru>
2020-02-20 14:51:03 +0500

    initial

---
+++ config/fonts.js
@@ -0,0 +1,48 @@
+module.exports = {
+	formats: 'local woff woff2',
+	display: "swap",
+	custom: {
+		"Montserat": {
+			variants: {
+				normal: {
+					300: {
+						url: {
+							woff: "../fonts/montserrat-light.woff",
+							woff2: "../fonts/montserrat-light.woff2"
+						}
+					},
+					400: {
+						url: {
+							woff: "../fonts/montserrat-regular.woff",
+							woff2: "../fonts/montserrat-regular.woff2"
+						}
+					},
+					500: {
+						url: {
+							woff: "../fonts/montserrat-semibold.woff",
+							woff2: "../fonts/montserrat-semibold.woff2"
+						}
+					},
+					700: {
+						url: {
+							woff: "../fonts/montserrat-bold.woff",
+							woff2: "../fonts/montserrat-bold.woff2"
+						}
+					}
+				}
+			}
+		},
+		"Gilroy": {
+			variants: {
+				normal: {
+					400: {
+						url: {
+							woff: "../fonts/Gilroy-Regular.woff",
+							woff2: "../fonts/Gilroy-Regular.woff2"
+						}
+					},
+				}
+			}
+		}
+	}
+}
---
+++ src/ts/common.ts
@@ -0,0 +1,31 @@
+import domReady from "./xpage/ready";
+import App from "./xpage/core";
+
+domReady(() => {
+
+	initMySelect();
+
+	const observerConfig: MutationObserverInit = {
+		childList: true,
+		subtree: true,
+	};
+
+	const obs = new MutationObserver((mutationList: Array<MutationRecord>, obs: MutationObserver) => {
+		for (const mutation of mutationList)
+			initMySelect();
+	});
+
+	obs.observe(document.body, observerConfig);
+});
+
+async function initMySelect(){
+	const selects = App.transformNodeListToArray(document.querySelectorAll(".default-input__input.default-input__input--select")) as Array<HTMLSelectElement>;
+
+	if (!selects.length) return;
+
+	const mySelect = (await import("./xpage/select")).default;
+
+	for (const select of selects)
+		if (!select.closest(".popup") && !select.MySelect)
+			new mySelect(select);
+}
